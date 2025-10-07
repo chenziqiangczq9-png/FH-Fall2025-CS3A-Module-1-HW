aa0147fbff6061068eb44748c57dec5319657e48
chenziqiangczq9-png <chenziqiangczq9@gmail.com>
2025-10-07 02:53:55 -0700

    Add files via upload

---
+++ AsstTwo.py
@@ -0,0 +1,16 @@
+"""Assignment Two: Greeting with f-strings"""
+
+
+def main():
+    """Prompt for the user's name and print a greeting using an f-string
+    """
+    my_name = input("Please enter your name ")
+    print(f"Hi {my_name}, let's explore some historical temperatures.")
+
+
+if __name__ == "__main__":
+    main()
+
+"""Please enter your name Zoe
+Hi Zoe, let's explore some historical temperatures.
+"""
